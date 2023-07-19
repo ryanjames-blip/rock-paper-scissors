80adaf44981311463fb07d2ff0db75916ef60ba0
Ryan <ryandenbleyker@gmail.com>
2023-07-19 11:41:50 -0400

    Add play functionality. TODO: getPlayerChoice

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
+// initialize play button and add event listener
+// to start the game
+
+const playButton = document.querySelector('#play-button');
+playButton.addEventListener('click', playGame);
+
+const display = document.querySelector('.display');
+display.textContent = 'Want to play a game?\nClick play to start';
+
+// initialize rock, paper and scissors buttons
+/*
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', )
+paper.addEventListener('click', )
+scissors.addEventListener('click', )
+*/
+const choices = document.querySelectorAll('.choice');
+
 //rock paper scissors logic below
 let playerWins = 0;
 let computerWins = 0;
-
+/*
 while (true) {
     
     playerWins = 0;
     computerWins = 0;
 
     //playGame();
-
-    //let playAgain = prompt('Play Again? (type \'y\' or \'n\'');
+    
+    let playAgain;// = prompt('Play Again? (type \'y\' or \'n\'');
     if (playAgain == 'n') {
         console.log('Thanks for playing!');
         break;
     }
+    
 }
-
+*/
 // generate a random number 
 // use that number to assign either rock, paper, or scissors
 function getComputerChoice() {
@@ -73,21 +95,31 @@ function playRound(computerSelection, playerSelection) {
 
 //simulate best of 5 match
 function playGame() {
-    while (computerWins < 3 && playerWins < 3) {
+    display.textContent = 'OK...\nLet\'s Play...First to 5 wins';
+    choices.forEach(choice => choice.addEventListener('click', getPlayerChoice));
+    while (computerWins < 5 && playerWins < 5) {
         let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Choose wisely: ");
-        playerSelection = playerSelection.toLowerCase();
+        let playerSelection = console.log(choice);
+        //playerSelection = playerSelection.toLowerCase();
         let result = playRound(computerSelection, playerSelection);
-        console.log(result);
+        display.textContent = result;
     }
 
-    if (computerWins == 3) {
-        console.log('Computer Wins!');
+    if (computerWins == 5) {
+        display.textContent = 'Computer Wins!';
     }
     else {
-        console.log('You win!')
+        display.textContent = 'You win!';
     }
 }
 // rock, paper, scissors logic above
 
+// TODO: complete function
+function getPlayerChoice(e) {
+    
+}
 
+/*
+listen for a click on rock, paper, and scissors
+assign player choice based on which is clicked
+*/
